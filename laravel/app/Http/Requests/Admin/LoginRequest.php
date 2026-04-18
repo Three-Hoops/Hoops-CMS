24830c6ddeeeaebb76dc89000b191b3634cc4487
Alex Verbraecken <64355830+iheartcore@users.noreply.github.com>
2026-04-18 10:26:30 +0200

    Updating code for last_login_at
    Updating code to fully enhance the new column

--- laravel/app/Http/Requests/Admin/LoginRequest.php
+++ laravel/app/Http/Requests/Admin/LoginRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Admin;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
@@ -28,16 +29,15 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if(! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))){
+        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
                 'email' => trans('auth.failed'),
             ]);
-        } else {
-            // successful login attempt
-            Auth::user()->update(['last_login_at' => now()]);
         }
+
+        User::query()->where('id', Auth::id())->update(['last_login_at' => now()]);
 
         RateLimiter::clear($this->throttleKey());
     }
